0ffe18554338d781679b369a55e59d2f779fbb9c
ducnguyen195 <duca23vietnam@gmail.com>
2023-08-21 15:03:21 +0700

    add file

---
+++ PHP/PHP-Đo An/DOAN/js/common.js
@@ -0,0 +1,51 @@
+const product = [
+    {
+        id:1,
+        name: 'Quả hồ đào Calo Nuts',
+        price: '160.000',
+        img:'product1.jpg',
+    },
+    {
+        id:2,
+        name: 'Hạt hạnh nhân Calo Nuts',
+        price: '150.000',
+        img:'product2.jpg',
+    },
+    {
+        id:3,
+        name: 'Hạt dẻ cười Calo Nuts',
+        price: '150.000',
+        img:'product3.jpg',
+    },
+    {
+        id:4,
+        name: 'Hạt mắc ca Úc Calo Nuts',
+        price: '200.000',
+        img:'product4.jpg',
+    },
+    {
+        id:5,
+        name: 'Hạt dẻ cười Calo Nuts',
+        price: '160.000',
+        img:'product5.jpg',
+    },
+    {
+        id:6,
+        name: 'Hạt điều Bình Phước Calo Nuts',
+        price: '170.000',
+        img:'product6.jpg',
+    },
+    {
+        id:7,
+        name: 'Hạt chia Calo Nuts',
+        price: '150.000',
+        img:'product7.jpg',
+    },
+    {
+        id:8,
+        name: 'Hạt óc chó Calo Nuts',
+        price: '160.000',
+        img:'product8.jpg',
+    },
+];
+
